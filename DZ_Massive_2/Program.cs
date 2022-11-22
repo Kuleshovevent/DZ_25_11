933c2307d644e8f839724b37441d2c6ce4f29a67
Anton <kuleshov.event@mail.ru>
2022-11-22 21:32:47 +0400

    Решение задач на семинаре

--- DZ_Massive_2/Program.cs
+++ DZ_Massive_2/Program.cs
@@ -1,12 +1,51 @@
 ﻿//Тренируемся 
-int[] array = new int [10];//new int создай новый массив в котором будет 10 элементов
-void FillArray (int[] collection)
+int[] array = new int [10];             //new int создай новый массив в котором будет 10 элементов
+void FillArray(int[] collection)
 {
     int lenght = collection.Length;
     int index = 0;
     while(index<lenght)
     {
-        collectoin(index)= new Random().Next(1,10);
+        collection[index]= new Random().Next(1,10);
         index++;
     }
 }
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;  //позиция
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int [] collection, int find)
+{
+    int count= collection.Length;
+    int index=0;
+    int position=0;
+    while (index < count)
+    {
+        if (collection[index]==find)
+        {
+            position==index;
+        }
+        
+        index++;
+
+    }
+    return position;
+}
+
+
+//int [] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos= IndexOf(array,4);
+Console.WriteLine(pos);
